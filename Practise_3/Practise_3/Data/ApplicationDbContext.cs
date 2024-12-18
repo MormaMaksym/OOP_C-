278de01c5698977f3c040e07dc86664dff43cd8c
MormaMaksym <st6793874@stud.duikt.edu.ua>
2024-12-18 14:02:46 +0200

    Add files via upload

---
+++ Practise_3/Practise_3/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practise_3;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+}
+
---
+++ Practise_3/Practise_3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practise_3
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+        }
+    }
+}
